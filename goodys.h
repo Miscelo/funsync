66b0d12c728b51d2f0dd38d50a515a67b65b7013
miscelo <miscelo@tutanota.com>
2022-11-20 10:48:00 +0100

    change Goodys library.

--- goodys.h
+++ goodys.h
@@ -3,13 +3,11 @@
 
 
 
-
-char *strxcat(char *str, ...);
-
-
 /*Function returns a pointer to a string of date and time -> format "20221107" */
 const char *getdate();
 
+/* Function write Backup status in file /var/log/syslog */
+void systemlog(int sysstate);
 
 /*Function devuelve la USER ID en un string. UID is a shell varible and not a environmental variable*/
 char *getUID();
