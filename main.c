939cb159789d2cdfa31bd36b5529e18faf183b5d
miscelo <miscelo@tutanota.com>
2022-12-15 16:13:46 +0100

    Added a linear list that stores all backup folder paths from a txt file. This txt file in future will be the base for all backups

--- main.c
+++ main.c
@@ -11,6 +11,29 @@
 #include <unistd.h>
 #include "goodys.h"
 
+#define max_pathlength 1023
+
+typedef struct _Sources_{
+    char source[max_pathlength];
+    struct _Sources_ *next;
+}Sources;
+
+typedef Sources *first;
+typedef Sources *List;
+
+/*Insert a new element to the list*/
+void insert(List *list, char sourceline[max_pathlength]);
+void copyFileToList(List *list, const char *sourcefile);
+
+
+void printList(List list){
+    printf("\n************     BACKUP FOLDERS    ***********\n");
+    while(list != NULL){
+        printf("%s\n", list->source);
+            list = list->next;
+    }
+    printf("************************************************\n");
+}
 
 
 int main(int argc, char *argv[]){
@@ -19,6 +42,13 @@ int main(int argc, char *argv[]){
     strcpy(dt, getdate());
     printf("Date: %s\n", dt);
     
+    /* Create a linear list that stores all backup-paths from a file*/
+    /* later , this list will be used in a loop to use rsync command*/
+    List list = NULL;
+    const char *sourcefile = "source.txt";
+    copyFileToList(&list, sourcefile);
+    printList(list);
+
     /* rsync -av  Transfer all files in Archive-Mode, ensures that devices, symbolic links , attributes, permissions,
        ownership are preserved in transfer. Also 'rsync -avz' will compressed it.
        rsync -avz /folder1 /folder2 /folder3 user@host::/home/user/backup   */
@@ -34,6 +64,51 @@ int main(int argc, char *argv[]){
 }
 
 
+/*Insert a new element to the list that stores all backup paths*/
+void insert(List *list, char sourceline[max_pathlength]){
+    first new, actual;
+    new = malloc(sizeof(Sources));
+    if(new == NULL){
+        fprintf(stderr, "Trouble to assign space on memory!\n");
+        exit(EXIT_FAILURE);
+    }
+    strcpy(new->source, sourceline);
+    if(*list == NULL){
+        new->next = *list;
+        *list = new;
+    } else {
+        actual = *list;
+        while(actual->next!=NULL){
+            actual = actual->next;
+        }
+        new->next = actual->next;
+        actual->next = new;
+    }
+}
+
+
+
+void copyFileToList(List *list, const char *sourcefile){
+    char line[max_pathlength];
+    const char nothing[1] = "";
+    FILE *fread = fopen(sourcefile, "r");  
+    if(fread == NULL){
+        fprintf(stderr, "Trouble to open file with backup source!\n");
+        exit(1);
+    }else{
+        while(fgets(line, sizeof(line), fread)!= NULL){
+            char *q = strchr(line, '\n'); /* Kick out every new line character.*/ 
+            if(q != NULL) *q = '\0';
+
+            char *p = strchr(line, '#');  /* Ignore line if line contains symbol '#'.*/
+            if((p == NULL) && (strcmp(nothing, line) != 0)){      /*kick out blanc lines.*/    
+                    insert(list, line);
+                
+            }
+        }
+    }
+    fclose(fread);
+}
 
 
 
